9023822601aae5d623abd7cae60e62d2b320dba8
ZaidAL02947505 <127364921+ZaidAL02947505@users.noreply.github.com>
2023-03-24 22:27:04 -0600

    Add files via upload

---
+++ Evidencia1/src/main/java/Clinica/Cita.java
@@ -0,0 +1,61 @@
+package Clinica;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Cita {
+    private String id;
+    private String fecha;
+    private String hora;
+    private String motivo;
+    private Doctor doctor;
+    private Paciente paciente;
+
+    public Cita(String id, String fecha, String hora, String motivo, Doctor doctor, Paciente paciente) {
+        this.id = id;
+        this.fecha= fecha;
+        this.hora = hora;
+        this.motivo = motivo;
+        this.doctor = doctor;
+        this.paciente = paciente;
+    }
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id=id;
+    }
+    public String getFecha() {
+        return fecha;
+    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public String getHora() {
+        return hora;
+    }
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+    public String getMotivo(){
+        return motivo;
+    }
+    public void setMotivo(String motivo){
+        this.motivo=motivo;
+    }
+    public Doctor getDoctor(){
+        return doctor;
+    }
+    public void setDoctor(Doctor doctor){
+        this.doctor=doctor;
+    }
+    public Paciente getPaciente(){
+        return paciente;
+    }
+    public void setPaciente(Paciente paciente){
+        this.paciente=paciente;
+    }
+
+    }
+
